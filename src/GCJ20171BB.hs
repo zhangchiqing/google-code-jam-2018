1e701966cfff7860239fa1c0613ec301ad190ab3
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2018-04-05 07:33:01 -0700

    solve 2017 R1B B small

--- src/GCJ20171BB.hs
+++ src/GCJ20171BB.hs
@@ -9,24 +9,17 @@ main :: IO ()
 main = interact runFile
 
 runFile :: String -> String
-runFile = unlines . zipWith format [1 ..] . map (uncurry6 solve) . parse
+runFile = unlines . zipWith format [1 ..] . map (uncurry7 solve) . parse
 
 parse :: String -> [(Int, Int, Int, Int, Int, Int, Int)]
-parse = parse' . tail . words
+parse = parse' . map read . tail . words
   where
     parse' [] = []
-    parse' (n_:r_:o_:y_:g_:b_:v_:xs) = (n, r, o, y, g, b, v) : parse' xs
-      where
-        n = read n_
-        r = read r_
-        o = read o_
-        y = read y_
-        g = read g_
-        b = read b_
-        v = read v_
+    parse' (n:r:o:y:g:b:v:xs) = (n, r, o, y, g, b, v) : parse' xs
 
-uncurry6 :: (a -> b -> c -> d -> e -> f -> g -> h) -> (a, b, c, d, e, f, g) -> h
-uncurry6 f' (a, b, c, d, e, f, g) = f' a b c d e f g
+-- try uncurryN
+uncurry7 :: (a -> b -> c -> d -> e -> f -> g -> h) -> (a, b, c, d, e, f, g) -> h
+uncurry7 f' (a, b, c, d, e, f, g) = f' a b c d e f g
 
 format :: Int -> String -> String
 format x y = "Case #" ++ show x ++ ": " ++ y
@@ -38,16 +31,25 @@ solve n r o y g b v =
     else stalls
   where
     stalls = buildWith is js ks
-    [(i, is), (j, js), (k, ks)] =
-      reverse $ sortOn fst [(r, rs), (y, ys), (b, bs)]
+    [is, js, ks] = reverse $ sortOn fst [(r, rs), (y, ys), (b, bs)]
     rs = replicate r 'R'
     ys = replicate y 'Y'
     bs = replicate b 'B'
 
-buildWith :: String -> String -> String -> String
-buildWith is js ks = takeEach is $ takeEach js ks
+buildWith :: (Int, String) -> (Int, String) -> (Int, String) -> String
+buildWith (i, is) (j, js) (k, ks) =
+  let ts = take j js ++ take (i - j) ks -- If I is the most, J is the second most, then first make a list of ITITITIT where T will first be J, then K if running out of J. To make a list of T, we need all the Js, and then as many Ks as possible, so that (len I) == (len ts)
+      (fillingK, remainingK) = splitAt (i - j) ks -- fillingK is the Ks used in T, remainingK is the left over Ks that are not being used as T
+      its = takeEach is ts -- make ITs with Is and Ts, so that it's a valid circle
+   in insertRemainingK its remainingK
 
-takeEach :: String -> String -> String
+-- insert remaining K to after T
+insertRemainingK :: Eq a => [a] -> [a] -> [a]
+insertRemainingK ijk [] = ijk
+insertRemainingK [] ks = ks
+insertRemainingK (i:jk:ijk) (k:ks) = i : jk : k : insertRemaining ijk ks
+
+takeEach :: Eq a => [a] -> [a] -> [a]
 takeEach xs [] = xs
 takeEach [] ys = ys
 takeEach (x:xs) (y:ys) = x : y : takeEach xs ys
@@ -71,14 +73,15 @@ tests =
     , testCase "solve" $ do
         solve 3 1 0 1 0 1 0 @?= "BYR"
         solve 3 2 0 1 0 1 0 @?= "RBRY"
-        solve 3 2 0 2 0 1 0 @?= "YRYBR"
-        solve 3 2 0 2 0 2 0 @?= "BYBRYR"
-        solve 6 2 0 2 0 2 0 @?= "BYBRYR"
+        solve 3 2 0 2 0 1 0 @?= "YRBYR"
+        solve 3 2 0 2 0 2 0 @?= "BYRBYR"
+        solve 6 2 0 2 0 2 0 @?= "BYRBYR"
         solve 3 1 0 2 0 0 0 @?= "IMPOSSIBLE"
-    -- , testCase "sample" $ do
-    --     input <- readFile "./cases/20171BB/in"
-    --     otput <- readFile "./cases/20171BB/out"
-    --     runFile input @?= otput
+        solve 7 3 0 3 0 1 0 @?= "YRBYRYR"
+    , testCase "sample" $ do
+        input <- readFile "./cases/20171BB/in"
+        otput <- readFile "./cases/20171BB/out"
+        runFile input @?= otput
     ]
 
 runTests = defaultMain tests
